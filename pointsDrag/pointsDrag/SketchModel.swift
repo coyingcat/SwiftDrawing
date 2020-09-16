f840aeceb589b95163200af7043bc329e38817fa
邓江洲 <707703386@qq.com>
2020-09-16 10:34:11 +0800

    pointsDrag

---
+++ pointsDrag/pointsDrag/SketchModel.swift
@@ -0,0 +1,29 @@
+//
+//  SketchModel.swift
+//  pointsDrag
+//
+//  Created by Jz D on 2020/9/16.
+//  Copyright © 2020 Jz D. All rights reserved.
+//
+
+import UIKit
+
+
+
+struct SketchModel{
+    var leftTop: CGPoint
+    var rightTop: CGPoint
+    var leftBottom: CGPoint
+    var rightBottom: CGPoint
+    
+    init() {
+        leftTop = CGPoint(x: 10, y: 10)
+        
+        rightTop = CGPoint(x: 100, y: 10)
+        
+        
+        leftBottom = CGPoint(x: 10, y: 100)
+        
+        rightBottom = CGPoint(x: 100, y: 100)
+    }
+}
---
+++ pointsDrag/pointsDrag/SketchView.swift
@@ -0,0 +1,201 @@
+//
+//  SketchView.swift
+//  pointsDrag
+//
+//  Created by Jz D on 2020/9/16.
+//  Copyright © 2020 Jz D. All rights reserved.
+//
+
+import UIKit
+
+
+enum SketchPointOption: Int{
+    case leftTop = 0, rightTop, rightBottom, leftBottom
+    
+}
+
+
+
+class SketchView: UIView {
+
+    var currentControlPointType: SketchPointOption?
+    
+    var lineLayer: CAShapeLayer = {
+        let l = CAShapeLayer()
+        l.fillColor = UIColor.clear.cgColor
+        l.strokeColor = UIColor.red.cgColor
+        return l
+    }()
+    
+    var pointsLayer: CAShapeLayer = {
+        let l = CAShapeLayer()
+        l.fillColor = UIColor.clear.cgColor
+        l.strokeColor = UIColor.green.cgColor
+        return l
+    }()
+    
+    var linePath: UIBezierPath = {
+        let l = UIBezierPath()
+        l.lineWidth = 2
+        return l
+    }()
+    
+    var pointPath: UIBezierPath = {
+        let l = UIBezierPath()
+        l.lineWidth = 2
+        return l
+    }()
+    
+    
+    var defaultPoints = SketchModel()
+    
+    
+
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        layer.addSublayer(lineLayer)
+        layer.addSublayer(pointsLayer)
+        
+        
+        
+    }
+    
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        lineLayer.frame = bounds
+        pointsLayer.frame = bounds
+        
+    }
+    
+    
+    
+    
+    /**
+     刷新数据
+     */
+    func reloadData(){
+        linePath.removeAllPoints()
+        pointPath.removeAllPoints()
+        draw(sketch: defaultPoints)
+        
+       
+        lineLayer.path = linePath.cgPath
+        pointsLayer.path = pointPath.cgPath
+    }
+    
+    
+    /**
+     绘制单个图形
+     */
+    func draw(sketch model: SketchModel){
+        drawLine(with: model)
+        drawPoints(with: model)
+    }
+
+    
+    
+    
+    
+    /**
+      绘制四条边
+     */
+    func drawLine(with sketch: SketchModel){
+        linePath.move(to: sketch.leftTop)
+        linePath.addLine(to: sketch.rightTop)
+        linePath.addLine(to: sketch.rightBottom)
+        linePath.addLine(to: sketch.leftBottom)
+        linePath.close()
+    }
+    
+    
+    
+    
+    /**
+     绘制四个顶点
+     */
+    func drawPoints(with sketch: SketchModel){
+        let radius: CGFloat = 8
+        pointPath.move(to: sketch.leftTop)
+        pointPath.addArc(withCenter: sketch.leftTop, radius: radius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
+        pointPath.move(to: sketch.rightTop)
+        pointPath.addArc(withCenter: sketch.rightTop, radius: radius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
+        pointPath.move(to: sketch.rightBottom)
+        pointPath.addArc(withCenter: sketch.rightBottom, radius: radius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
+        pointPath.move(to: sketch.leftBottom)
+        pointPath.addArc(withCenter: sketch.leftBottom, radius: radius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
+    }
+    
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        super.touchesBegan(touches, with: event)
+        
+        
+        guard let touch = touches.first else{
+            return
+        }
+        let currentPoint = touch.location(in: self)
+        
+        // 判定选中的最大距离
+        let maxDistance: CGFloat = 20
+        let points = [defaultPoints.leftTop, defaultPoints.rightTop, defaultPoints.rightBottom, defaultPoints.leftBottom]
+        for pt in points{
+            let distance = sqrt(pow(pt.x - currentPoint.x, 2) + pow(pt.y - currentPoint.y, 2))
+            if distance <= maxDistance{
+                if let pointIndex = points.firstIndex(of: pt){
+                    currentControlPointType = SketchPointOption(rawValue: pointIndex)
+                    break
+                }
+            }
+        }
+    }
+    
+
+
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        if let current = currentControlPointType, let touch = touches.first{
+            let pt = touch.location(in: self)
+            guard bounds.contains(pt) else{
+                return
+            }
+            switch current {
+            case .leftTop:
+                defaultPoints.leftTop = pt
+            case .rightTop:
+                defaultPoints.rightTop = pt
+            case .leftBottom:
+                defaultPoints.leftBottom = pt
+            case .rightBottom:
+                defaultPoints.rightBottom = pt
+            }
+            reloadData()
+        }
+        
+    }
+
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        currentControlPointType = nil
+    }
+    
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        currentControlPointType = nil
+    }
+}
