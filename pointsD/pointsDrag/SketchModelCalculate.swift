d3549ba627bb3457162c279a7d59c3d6fb220971
邓江洲 <707703386@qq.com>
2020-09-27 10:01:56 +0800

    in order

--- pointsD/pointsDrag/SketchModelCalculate.swift
+++ pointsD/pointsDrag/SketchModelCalculate.swift
@@ -22,6 +22,78 @@ typealias ReturnTopCenter = (lhsTop: CGPoint, rhsTop: CGPoint)
 extension CGPoint{
     
 
+    
+    
+    
+    func calculateTopCenter(lhsTopP lhsTop: CGPoint, rhsTopP rhsTop: CGPoint, rhsBottomP rhsBottom: CGPoint, lhsBottomP lhsBottom: CGPoint) -> ReturnTopCenter{
+        
+        var result: ReturnTopCenter = (CGPoint.zero, CGPoint.zero)
+        
+        let topDistance = rhsTop.x - lhsTop.x
+        var lhsV = true, rhsV = true
+        
+        
+        
+        let lhsDistance = lhsTop.x - lhsBottom.x
+        if abs(lhsDistance) < SketchConst.std.limite{
+            lhsV = false
+        }
+        let rhsDistance = rhsTop.x - rhsBottom.x
+        if abs(rhsDistance) < SketchConst.std.limite{
+            rhsV = false
+        }
+        
+        
+        
+        let k = (rhsTop.y - lhsTop.y)/topDistance
+        
+        //  (a-y)/w-(c-y)/k=b-d
+        
+        // 求解 y
+        
+        //  -(((d-b)*k-c)*w+a*k)/(w-k)
+        
+        //  (((b - d)*k + c)*w - a*k)/(w - k)
+        
+        //  (((d-b)*k-c)*w+a*k)/(k - w)
+        
+        ////
+        
+        ///
+        
+        //  (a-y)/w=b-x
+        
+        //  求解 x
+        
+        //  x = (y+b*w-a)/w
+        
+        
+        if lhsV{
+            let w = (lhsTop.y - lhsBottom.y)/lhsDistance
+            let retY = (((lhsBottom.x - x) * k + y) * w - lhsBottom.y * k)/(w - k)
+            result.lhsTop = CGPoint(x: (retY + lhsBottom.x * w - lhsBottom.y)/w, y: retY)
+        }
+        else{
+            let resultY = (lhsBottom.x - x) * k + y
+            result.lhsTop = CGPoint(x: x, y: resultY)
+        }
+        
+        if rhsV{
+            let rhs = (rhsTop.y - rhsBottom.y)/rhsDistance
+            let rhsY = (k * rhsBottom.y + ((x - rhsBottom.x)*k - y) * rhs)/(k - rhs)
+            result.rhsTop = CGPoint(x: (rhsY + rhsBottom.x * rhs - rhsBottom.y)/rhs, y: rhsY)
+        }
+        else{
+            let rhsY = y - (x - rhsBottom.x)*k
+            result.rhsTop = CGPoint(x: x, y: rhsY)
+        }
+        
+        return result
+    }
+    
+    
+    
+    
         
     
     func calculateRightCenter(lhsTopP lhsTop: CGPoint, rhsTopP rhsTop: CGPoint, rhsBottomP rhsBottom: CGPoint, lhsBottomP lhsBottom: CGPoint) -> ReturnRightCenter{
@@ -159,74 +231,6 @@ extension CGPoint{
         return result
     }
     
-    
-    
-    
-    func calculateTopCenter(lhsTopP lhsTop: CGPoint, rhsTopP rhsTop: CGPoint, rhsBottomP rhsBottom: CGPoint, lhsBottomP lhsBottom: CGPoint) -> ReturnTopCenter{
-        
-        var result: ReturnTopCenter = (CGPoint.zero, CGPoint.zero)
-        
-        let topDistance = rhsTop.x - lhsTop.x
-        var lhsV = true, rhsV = true
-        
-        
-        
-        let lhsDistance = lhsTop.x - lhsBottom.x
-        if abs(lhsDistance) < SketchConst.std.limite{
-            lhsV = false
-        }
-        let rhsDistance = rhsTop.x - rhsBottom.x
-        if abs(rhsDistance) < SketchConst.std.limite{
-            rhsV = false
-        }
-        
-        
-        
-        let k = (rhsTop.y - lhsTop.y)/topDistance
-        
-        //  (a-y)/w-(c-y)/k=b-d
-        
-        // 求解 y
-        
-        //  -(((d-b)*k-c)*w+a*k)/(w-k)
-        
-        //  (((b - d)*k + c)*w - a*k)/(w - k)
-        
-        //  (((d-b)*k-c)*w+a*k)/(k - w)
-        
-        ////
-        
-        ///
-        
-        //  (a-y)/w=b-x
-        
-        //  求解 x
-        
-        //  x = (y+b*w-a)/w
-        
-        
-        if lhsV{
-            let w = (lhsTop.y - lhsBottom.y)/lhsDistance
-            let retY = (((lhsBottom.x - x) * k + y) * w - lhsBottom.y * k)/(w - k)
-            result.lhsTop = CGPoint(x: (retY + lhsBottom.x * w - lhsBottom.y)/w, y: retY)
-        }
-        else{
-            let resultY = (lhsBottom.x - x) * k + y
-            result.lhsTop = CGPoint(x: x, y: resultY)
-        }
-        
-        if rhsV{
-            let rhs = (rhsTop.y - rhsBottom.y)/rhsDistance
-            let rhsY = (k * rhsBottom.y + ((x - rhsBottom.x)*k - y) * rhs)/(k - rhs)
-            result.rhsTop = CGPoint(x: (rhsY + rhsBottom.x * rhs - rhsBottom.y)/rhs, y: rhsY)
-        }
-        else{
-            let rhsY = y - (x - rhsBottom.x)*k
-            result.rhsTop = CGPoint(x: x, y: rhsY)
-        }
-        
-        return result
-    }
     
     
     
